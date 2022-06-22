73dec334ad27ace872459c6a513b80847c573d92
Ali Sa <alex.technosun@gmail.com>
2022-06-22 11:37:56 -0700

    index.html

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -2,35 +2,38 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
-# Create an instance of our Flask app.
+# Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
+
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def index():
 
     # Find one record of data from the mongo database
-    mars_data = mongo.db.collection.find_one()
+    mars_record = mongo.db.collection.find_one()
 
     # Return template and data
-    return render_template("index.html", mars_dict=mars_data, text="Scraping New Data")
+    # mars = list(db.mars.find())
+    return render_template("index.html", final_dictionary=mars_record, text="Scrape New Data!")
+
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
 
     # Run the scrape function
-    mars_dict = scrape_mars.scrape_info()
+    final_dictionary = scrape_mars.scrape()
 
     # Insert the record
-    mongo.db.collection.update_one({}, {"$set": mars_dict}, upsert=True)
-
-     # Redirect back to home page
+    mongo.db.collection.update_one({}, {"$set": final_dictionary}, upsert=True)
+    
+    # Redirect back to home page
     return redirect("/")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
